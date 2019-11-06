352876b06458ebbd191c4a5d51fd022ef0c2f7a5
BramBrooks <bram451@hotmail.com>
2019-11-06 12:29:03 +0000

    Created basic MessageInput functionality

--- src/Components/MessageInput.jsx
+++ src/Components/MessageInput.jsx
@@ -1,12 +1,40 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 
 class MessageInput extends PureComponent {
   state = {
-    messageInput: ''
+    messageInput: ""
+  };
+
+  handleChange = event => {
+    this.setState({ messageInput: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.messageSubmit(this.state.messageInput);
+    this.setState({ messageInput: "" });
   };
 
   render() {
-    return <p>Message input box goes here</p>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label name="item-input">Message here:</label>
+
+          <input
+            className="inputBox"
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.messageInput}
+            name="item-input"
+            maxLength="200"
+            autoComplete="off"
+            size="50"
+          />
+        </p>
+        <input type="submit"></input>
+      </form>
+    );
   }
 }
 
